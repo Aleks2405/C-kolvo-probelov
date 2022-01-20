a9d467c5d6ae2e69cc008519ea3cf614f9064425
Aleksandr <shura.gorodnichev@mail.ru>
2022-01-21 00:28:46 +0300

    Добавьте файлы проекта.

---
+++ количество пробелов/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/*Написать программу, которая считывает символы с клавиатуры, 
+ * пока не будет введена точка. Программа должна
+сосчитать количество введенных пользователем пробелов.*/
+
+namespace количество_пробелов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "", s = "";
+            int count = 0;
+
+            while (a != ".")
+            {
+                s += a;
+                a = Console.ReadLine();
+            }
+            Console.WriteLine(s);
+            
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
